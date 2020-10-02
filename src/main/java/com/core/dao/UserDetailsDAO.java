bd6f64212981e5111a85fdeebbe032101e9243ed
Akhil Daliparthi <akhilaki654@gmail.com>
2020-10-02 11:39:42 +0530

    User Details DAO update

--- src/main/java/com/core/dao/UserDetailsDAO.java
+++ src/main/java/com/core/dao/UserDetailsDAO.java
@@ -1,6 +1,5 @@
 package com.core.dao;
 
-import com.core.components.Person;
 import com.core.model.User;
 import com.core.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +10,10 @@ import java.util.List;
 @Component
 public class UserDetailsDAO {
 
-    public List<Person> personList;
-
-    @Autowired
-    UserDetailsDAO(List<Person> personList){
-        this.personList = personList;
-    }
-
     @Autowired
     private UserRepository userRepository;
 
     public List<User> getData() {
         return userRepository.findAll();
-    }
-
-    public List<Person> getPersonList(){
-        return this.personList;
     }
 }
